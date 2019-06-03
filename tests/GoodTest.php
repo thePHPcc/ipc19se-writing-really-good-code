1cd7197021eece5770ae97ce87d4883f4dd46e73
Markus Unterkofler <30629050+MarkusCooks@users.noreply.github.com>
2019-06-03 15:02:31 +0200

    Added tests for further goods

--- tests/GoodTest.php
+++ tests/GoodTest.php
@@ -9,10 +9,45 @@ use PHPUnit\Framework\TestCase;
  */
 final class GoodTest extends TestCase
 {
+    public function testCanBeWool(): void
+    {
+        $wool = Good::wool();
+
+        $this->assertTrue($wool->isWool());
+    }
+
+    public function testCanBeGrain(): void
+    {
+        $grain = Good::grain();
+
+        $this->assertTrue($grain->isGrain());
+    }
+
     public function testCanBeMilk(): void
     {
         $milk = Good::milk();
 
         $this->assertTrue($milk->isMilk());
+    }
+
+    public function testCanBeBread(): void
+    {
+        $bread = Good::bread();
+
+        $this->assertTrue($bread->isBread());
+    }
+
+    public function testCanBeCheese(): void
+    {
+        $cheese = Good::cheese();
+
+        $this->assertTrue($cheese->isChesse());
+    }
+
+    public function testCanBeWhisky(): void
+    {
+        $whisky = Good::whisky();
+
+        $this->assertTrue($whisky->isWhisky());
     }
 }
