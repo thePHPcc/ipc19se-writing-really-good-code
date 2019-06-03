e6098261e0eba708f0bc5ca464242bd326624ba7
Sebastian Bergmann <sebastian@thephp.cc>
2019-06-03 10:30:16 +0200

    Initial work on Pound object

---
+++ src/Pound.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+namespace ClansOfCaledonia;
+
+final class Pound
+{
+    public function amount(): int
+    {
+        return 1234;
+    }
+}
---
+++ tests/PoundTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+namespace ClansOfCaledonia;
+
+use PHPUnit\Framework\TestCase;
+
+final class PoundTest extends TestCase
+{
+    public function testHasAmount(): void
+    {
+        $amount = 1234;
+
+        $p = new Pound($amount);
+
+        $this->assertSame($amount, $p->amount());
+    }
+}
